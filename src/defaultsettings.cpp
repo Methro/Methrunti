9e19b844dead6534e4aa7747834ae0b5376713d2
Methro <root@sandrac-desktop>
2025-02-17 10:47:04 -0500

    Maybe reverting?

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -245,7 +245,7 @@ void set_default_settings()
 	settings->setDefault("fps_max", "60");
 	settings->setDefault("fps_max_unfocused", "10");
 	settings->setDefault("viewing_range", "190");
-	settings->setDefault("client_mesh_chunk", "0");
+	settings->setDefault("client_mesh_chunk", "1");
 	settings->setDefault("screen_w", "1024");
 	settings->setDefault("screen_h", "600");
 	settings->setDefault("window_maximized", "false");
